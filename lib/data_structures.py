7b14ca6c7c9f28baacaf10a01d01703ffeda9d0b
joy Kurui <kurui.chepngetich@gmail.com>
2023-12-13 19:08:34 +0300

    Completed Assignment

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,62 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    return [food.get("name") for food in spicy_foods]
+
+# names = get_names(spicy_foods)
+# print(names)
 
 def get_spiciest_foods(spicy_foods):
-    pass
-
+    return [food for food in spicy_foods if food["heat_level"] > 5]
+       
 def print_spicy_foods(spicy_foods):
-    pass
+    spicy = []
+    for food in spicy_foods:
+        name = food["name"]
+        cuisine = food["cuisine"]
+        heat_level = food["heat_level"]
+        emoji = "🌶" * heat_level
+        spicy.append(f"{name} ({cuisine}) | Heat Level: {emoji}")
+
+    for item in spicy:
+        print(item)
+print_spicy_foods(spicy_foods)
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food 
+    return None 
+result_american = get_spicy_food_by_cuisine(spicy_foods, "American")
+print(result_american)      
+
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    spiciest = []
+    for food in spicy_foods:
+        name = food["name"]
+        cuisine = food["cuisine"]
+        heat_level = food["heat_level"]
+        emoji = "🌶" * heat_level
+        if heat_level > 5:
+            spiciest.append(f"{name} ({cuisine}) | Heat Level: {emoji}")
+
+    for item in spiciest:
+        print(item)
+print_spiciest_foods(spicy_foods)
 
 def get_average_heat_level(spicy_foods):
-    pass
+    heat_levels=[]
+    for food in spicy_foods:
+        heat_level = food["heat_level"]
+        heat_levels.append(heat_level)
+    # print(heat_levels)
+    avg = sum(heat_levels)/len(heat_levels)
+    return avg
+average = get_average_heat_level(spicy_foods)
+print(average)
+
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
